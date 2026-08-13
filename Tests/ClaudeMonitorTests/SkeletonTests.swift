91bda4e710462d10d8e0f3e310eb4e07a1abfff1
Albert Nguyen <albertng.dev@gmail.com>
2026-08-13 15:17:15 +0700

    Update skeleton test for DropdownView(viewModel:) initializer

--- Tests/ClaudeMonitorTests/SkeletonTests.swift
+++ Tests/ClaudeMonitorTests/SkeletonTests.swift
@@ -2,8 +2,9 @@ import XCTest
 @testable import ClaudeMonitor
 
 final class SkeletonTests: XCTestCase {
+    @MainActor
     func test_dropdownView_instantiates_andBodyEvaluatesWithoutCrashing() {
-        let view = DropdownView()
+        let view = DropdownView(viewModel: MonitorViewModel())
         _ = view.body
     }
 }
